60161dfeca05f03457ac680f44c602a127dee168
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-08-13 00:56:59 -0400

    completed index.html, readme.md, and added comments

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,87 +3,195 @@ from splinter import Browser
 from config import exe_path
 import pandas as pd
 import requests
+import time
 
 def scrape ():
+    # Initialize the dictionary that will be returned
     mars_scrape = {}
 
+    # Inititate Splinter browser
     executable_path = {'executable_path':exe_path}
     browser = Browser('chrome', **executable_path)
 
-    # Nasa Mars News
+    # -----------------------#
+    #     Nasa Mars News     #
+    # -----------------------#
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
 
+    # pause to give the page time to load
+    time.sleep(2)
+
+    # Pull html code from web page and allow BeautifulSoup to parser through the code
     html = browser.html
     soup = bs(html, 'html.parser')
 
+    # pause to give the parser time to parse
+    time.sleep(3)
+
+    # Search for the first article on the page and pull the title and synopsis paragraph
     results = soup.find('div', class_="list_text")
     news_title = results.find('a').text.strip('\n')
     news_p = soup.find('div', class_="article_teaser_body").text
 
+    # Add the article title and paragraph to the dictionary that is to be returned
     mars_scrape['news_title'] = news_title
     mars_scrape['news_p'] = news_p
 
-    
-    # JPL Mars Space Image
+    # -----------------------#
+    #  JPL Mars Space Image  #
+    # -----------------------#
+    # Load next url
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
 
+    # Pull html code from web page and allow BeautifulSoup to parser through the code    
     html = browser.html
     soup = bs(html, 'html.parser')
 
+    # Click on the link to the featured image
     browser.click_link_by_id("full_image")
+
+    # pause to give the page time to load
+    time.sleep(2)
+
+    # Pull html code from web page and allow BeautifulSoup to parser through the code
     html = browser.html
     soup = bs(html, 'html.parser')
 
+    # pause to give the parser time to parse
+    time.sleep(3)
+
+    # Find the link to the description page for the featured image & click the link
     results = soup.find('a', class_="button", target="_top")
     partial_href = results['href']
     browser.click_link_by_partial_href(partial_href)
+    
+    # Pull html code from web page and allow BeautifulSoup to parser through the code
     html = browser.html
     soup = bs(html, 'html.parser')
+
+    # Find the featured image link and save the link
     results = soup.find('figure', class_="lede")
     link = results.find('a')
     partial_href = link['href']
+
+    # break apart the url used to get to this page to extract the first part of it
     url_head = url.split('?')
     url_head2 = url_head[0].rsplit('/', maxsplit=2)
+
+    # combine the first part of the base url and the link to the feature image together
     featured_image_url = url_head2[0] + partial_href
 
+    # Add the scraped information from the url to the dictionary that is to be returned
     mars_scrape['featured_image_url'] = featured_image_url
 
-    # Mars Weather
+    # -----------------------#
+    #      Mars Weather      #
+    # -----------------------#
+    # Load the next url
     url = 'https://twitter.com/marswxreport?lang=en'
     browser.visit(url)
+
+    # pause to give the page time to load
+    time.sleep(2)
+
+    # Pull html code from web page and allow BeautifulSoup to parser through the code
     html = browser.html
     soup = bs(html, 'html.parser')
+
+    # pause to give the parser time to parse
+    time.sleep(2)
+
+    # Find the desired information
     results = soup.find('div', class_="css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-1mi0q7o")
     spans = results.find_all('span')
     mars_weather = spans[4].text
 
+    # Add the scraped information from the url to the dictionary that is to be returned
     mars_scrape['mars_weather'] = mars_weather
 
-    # Mars Facts
+    # -----------------------#
+    #       Mars Facts       #
+    # -----------------------#
+    # Load the next url
     url = 'https://space-facts.com/mars/'
     browser.visit(url)
-    tables = pd.read_html(url)
-    df = tables[0]
-    df.columns = ['Name', 'Value']
-    df.set_index('Name', inplace=True)
-    html_string = df.to_html()
-    html_string = html_string.replace('\n', '')
 
+    # put the tables on the page into a list of Dataframes
+    tables = pd.read_html(url)
+
+    # Extract the desired DataFrame (there is only one on this page)
+    df = tables[0]
+
+    # Name the DataFrame columns
+    df.columns = ['Name', 'Value']
+
+    # Reset the index to be the Name columns
+    df.set_index('Name', inplace=True)
+
+    # Convert the Dataframe into an html table
+    html_string = df.to_html()
+
+    # Strip the \n characters out of the html table code
+    html_string = html_string.replace('\n', '')
+    html_string = html_string.replace(' style="text-align: right;"', '')
+
+    # Add the scraped information from the url to the dictionary that is to be returned
     mars_scrape['html_string'] = html_string
 
-    # Mars Hemispheres
+    # -----------------------#
+    #    Mars Hemispheres    #
+    # -----------------------#
+    # Load the next url
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
+
+    # Initialize an empty list to hold the scraped information
     hemisphere_image_urls = []
+
+    # Pull html code from web page and allow BeautifulSoup to parser through the code
     html = browser.html
     soup = bs(html, 'html.parser')
+
+    # break apart the url used to get to this page to extract the first part of it
+    base_url = url.split('/search/')
+    base_url[0]
+
+    # Find the information for the four hemispheres
     results = soup.find_all('div', class_="item")
-    partial_href = results[0].find('a')['href']
+    
+    # loop through the four items in the results list
+    for x in range(0,4):
+        # Extract the name of the hemisphere and strip off the word Enhanced
+        raw_title = results[x].find('h3').text
+        title = raw_title.replace(" Enhanced", "")
 
-    # need more code here to finish this section
+        # Extract the second part of the url then join the two parts together
+        partial_href = results[x].find('a')['href']
+        full_href = base_url[0] + partial_href
 
+        # Vist the url that was just created        
+        browser.visit(full_href)
+
+        # Pull html code from web page and allow BeautifulSoup to parser through the code
+        html = browser.html
+        soup = bs(html, 'html.parser')
+
+        # Search the new results for the image url
+        results2 = soup.find_all('div', class_="wide-image-wrapper")
+        link = results2[0].find('img', class_="wide-image")
+        href = link['src']
+        full_url = base_url[0] + href
+
+        # Add the name and image url to the list as a dictionary
+        hemisphere_image_urls.append({"title":title, "img_url":full_url})
+
+    # Add the scraped information from the url to the dictionary that is to be returned
     mars_scrape['hemisphere_image_urls'] = hemisphere_image_urls
 
+    # close the browser
+    browser.quit()
+
+    # Return the dictionary of all the scraped information
     return(mars_scrape)
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # web-scraping-challenge
+
+before running app.py:
+In gitbash:
+> conda activate PythonData
+> "C:\Program Files\MongoDB\Server\4.4\bin\mongod.exe"
+Open MongoDB Compass
+- select "localhost:27017" under recent connections on the left
+- Click "Connect" (green button)
+
+To run app.py:
+In gitbash:
+> conda activate PythonData         <!-- if you haven't already -->
+> cd <path to where app.py lives>
+> python app.py
+...
+* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit) <!-- user can click on http link to open a new browser -->
+
+Once the browser is open:
+* user can click the scrape button to get the most recent mars data
+* another browser will open and navigate to multiple pages, collecting the data
+* this process can take a minute or two DO NOT close the new browser
+* once the data is collected the new browser will close on its own
+* finally, the homepage will automatically reload with the new data
